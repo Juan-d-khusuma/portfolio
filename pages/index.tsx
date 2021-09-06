7fd4335049c3673e027806a8f29ff716055535ff
Juan D.K <66712975+Juan-d-khusuma@users.noreply.github.com>
2021-09-06 15:17:00 +0700

    fixes/I dont think storybook is needed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import React from "react";
-import MatrixText from "./components/MatrixTextEffect/index";
+import MatrixText from "../components/MatrixTextEffect/index";
 
 /**
  * @return {JSX.Element}
